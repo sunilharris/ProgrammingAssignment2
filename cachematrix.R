d26c66b10e538ac8a90bc5fbd0e5bf5d67b5f1cf
sunilharris <sunil.harris@gmail.com>
2015-09-21 15:40:36 +0530

    Peer Assessment in R
    Peer Assessment in R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Program that will caclulate the inverse of the matrix
+## If the matri has already been used before, it will pull from cache, else
+## build cache and inverse
 
-## Write a short comment describing this function
-
+## getter setter for the cachcing the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        o_Inv <- NULL
+        set <- function(y) {
+                x <<- y
+                o_Inv <<- NULL
+        }
+        get <- function() x
+        fnsetinverse <- function(oInv) o_Inv <<- oInv
+        fngetinverse <- function() o_Inv
+        list(set = set, get = get,
+             fnsetinverse = fnsetinverse,
+             fngetinverse = fngetinverse)
 }
 
 
-## Write a short comment describing this function
-
+## solve command to inverse the matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        # checking if the cached value is present
+        o_Inv <- x$fngetinverse()
+        # if the value is cached return the cached value
+        if(!is.null(o_Inv)) {
+                message("getting cached data")
+                return(o_Inv)
+        }
+        data <- x$get()
+        #print(data)
+        #calculating the inverse
+        o_Inv <- solve(data, ...)
+        #setting the value to cache
+        x$fnsetinverse(o_Inv)
+        #returning the inverse
+        o_Inv
 }
